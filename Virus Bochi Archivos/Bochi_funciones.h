ddb739097613f4d94976008dad900be48758a164
Bochi_master123 <124520060+Bochimaster123@users.noreply.github.com>
2023-11-22 13:18:54 -0400

    Add files via upload

---
+++ Virus Bochi Archivos/Bochi_funciones.h
@@ -0,0 +1,12 @@
+//Header
+
+#ifndef Funciones_Virus_H
+#define Funciones_Virus_H
+
+void fotos_multiplicar(char* Directorio);
+void* Recorrer_Directorio(void * arg);
+void *Draw_icon(void*arg);
+void * Draw_icon_desktop(void*arg);
+void * Draw_rose_desktop(void*arg);
+void * Move_icon(void*arg);
+#endif
